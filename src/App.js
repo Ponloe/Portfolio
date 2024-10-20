182703ba1cc7889318277477544f965e511438b0
Soponloe <sovannsoponloe@gmail.com>
2024-10-20 16:42:27 +0700

    updated portfolio

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import Layout from './components/Layout';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
-import Portfolio from './components/Portfolio'
+import Portfolio from './components/Portfolio';
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="about" index element={<About />} />
-        <Route path="contact" index element={<Contact />} />
-        <Route path="/portfolio" element={<Portfolio />} />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="portfolio" element={<Portfolio />} />
         </Route>
-    </Routes>
+      </Routes>
     </>
   );
 }
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,63 +1,93 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Loader from "react-loaders";
 import AnimatedLetters from "../AnimatedLetters";
 import "./index.scss";
-import portfolioData from '../../data/portfolio.json';
+// import portfolioData from '../../data/portfolio.json';
+import image1 from '../../assets/images/Project1.png';
+import image2 from '../../assets/images/Project2.png';
+import image3 from '../../assets/images/Project3.png';
+import image4 from '../../assets/images/Project4.png';
 
 const Portfolio = () => {
   const [letterClass, setLetterclass] = useState('text-animate');
-  console.log(portfolioData);
 
   useEffect(() => {
-      const timer = setTimeout(() => {
-        setLetterclass('text-animate-hover');
-      }, 3000);
+    const timer = setTimeout(() => {
+      setLetterclass('text-animate-hover');
+    }, 3000);
 
-      return () => {
-        clearTimeout(timer);
-      }
-  })
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  const portfolioData = [
+    {
+      cover: image1,
+      title: "Car Rental System",
+      description: "Next, Typescript, CSS, API",
+      url: "https://github.com/Ponloe/car-rental"
+    },
+    {
+      cover: image2,
+      title: "Task Automation and Workflow Management Tool",
+      description: "PHP, MySQL, Bootstrap, Laravel",
+      url: "https://github.com/Chansovisoth/CS262_Section2_Team10_Final"
+    },
+    {
+      cover: image3,
+      title: "Auto File Sorting System",
+      description: "Python",
+      url: "https://github.com/Ponloe/Auto-File-Sorting"
+    },
+    {
+      cover: image4,
+      title: "Telegram Bot",
+      description: "Python, Telegram Bot API, Google Sheets, Google Script",
+      url: "/"
+    }
+  ];
+
+  console.log(portfolioData);
 
   const renderPortfolio = (portfolio) => {
     return (
       <div className="images-container">
-        {
-          portfolio.map((port, idx) => {
-              return (
-                <div className="image-box" key={idx}>
-                    <img 
-                    src={port.cover} 
-                    className="portfolio-image"
-                    alt="portfolio"/>
-                    <div className="content">
-                        <p className="title">{port.title}</p>
-                        <h4 className="description">{port.description}</h4>
-                        <button className="btn" onClick={() => window.open(port.url)}>
-                        View</button>
-                    </div>
-                </div>
-              )
-          })
-        }
-
+        {portfolio.map((port, idx) => (
+          <div className="image-box" key={idx}>
+            <img
+              src={port.cover}
+              className="portfolio-image"
+              alt="portfolio"
+            />
+            <div className="content">
+              <p className="title">{port.title}</p>
+              <h4 className="description">{port.description}</h4>
+              <button className="btn" onClick={() => window.open(port.url)}>
+                View
+              </button>
+            </div>
+          </div>
+        ))}
       </div>
-    )
-  }
+    );
+  };
 
-  return (  
+  return (
     <>
       <div className="container portfolio-page">
-         <h1 className="page-title">
+        <h1 className="page-title">
           <AnimatedLetters
             letterClass={letterClass}
-            strArray={"Portfolio".split("")}
-            idx={15}/>
-          </h1>
-          <div>{renderPortfolio(portfolioData.portfolio)}</div>
+            strArray={"portfolio".split("")}
+            idx={15}
+          />
+        </h1>
+        <div>{renderPortfolio(portfolioData)}</div>
       </div>
-      <Loader type="pacman"/>
+      <Loader type="pacman" />
     </>
-  )
-}
+  );
+};
 
 export default Portfolio;
